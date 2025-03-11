8696e7066135f2d56b4a4ed841160a56a2c5b385
usrlocal <usrlocal@GA213-FE22.ad.univ-angers.fr>
2025-03-11 11:11:00 +0100

    fichiers dans le bon dossier pour node.js

--- monserveur.js
+++ monserveur.js
@@ -1,13 +1,21 @@
 var express = require('express');
+var path = require('path');
 var app = express();
- 
+
+// Servir les fichiers statiques (CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pour la page d'accueil
 app.get('/accueil', function(req, res) {
-  res.sendFile(__dirname + "/Calendier.html");
+  res.sendFile(path.join(__dirname, "Calendier.html"));
 });
- 
+
+// Route pour l'autre page
 app.get('/autre', function(req, res) {
-  res.sendFile(__dirname + "/Edit.html");
+  res.sendFile(path.join(__dirname, "Edit.html"));
 });
- 
-app.listen(8000);
-console.log("App listening on port 8000...");
+
+// Lancement du serveur sur le port 8000
+app.listen(8000, () => {
+  console.log("App listening on port 8000...");
+});
