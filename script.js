cb3590a1ac1929c600d3faf1085ef108f2072ad5
Charly <charly.gr2003@mail.com>
2025-03-11 11:56:28 +0100

    update1.0 pages html

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ function clearSections() {
     document.getElementById('contact').innerHTML = '';
 }
 
-function loadCalendrier() {
+function loadPage(page) {
     // Vider les sections avant de charger le nouveau contenu
     clearSections();
 
+    let chemin = "http://localhost:3000/" + page.toString() + ".html";
+
     // Charger le contenu de calendrier.html
-    fetch('http://localhost:3000/calendrier.html')
+    fetch(chemin)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erreur de chargement du fichier calendrier.html');
@@ -46,10 +48,38 @@ function loadCalendrier() {
 // Ajouter l'écouteur d'événements pour le lien Calendrier
 document.addEventListener('DOMContentLoaded', function () {
     const calendrierLink = document.querySelector('a[href="?a=calendrier"]');
+    const editLink = document.querySelector('a[href="?a=edit"]');
+    const connexionLink = document.querySelector('a[href="?a=connexion"]');
+    const clubsLink = document.querySelector('a[href="?a=clubs"]');
+    const newsLink = document.querySelector('a[href="?a=news"]');
     if (calendrierLink) {
         calendrierLink.addEventListener('click', function (e) {
             e.preventDefault();  // Empêcher le comportement par défaut du lien
-            loadCalendrier();    // Charger le calendrier lorsqu'on clique
+            loadPage("calendrier");    // Charger le calendrier lorsqu'on clique
+        });
+    }
+    if (editLink) {
+        editLink.addEventListener('click', function (e) {
+            e.preventDefault();  // Empêcher le comportement par défaut du lien
+            loadPage("edit");    // Charger le calendrier lorsqu'on clique
+        });
+    }
+    if (connexionLink) {
+        connexionLink.addEventListener('click', function (e) {
+            e.preventDefault();  // Empêcher le comportement par défaut du lien
+            loadPage("connexion");    // Charger le calendrier lorsqu'on clique
+        });
+    }
+    if (clubsLink) {
+        clubsLink.addEventListener('click', function (e) {
+            e.preventDefault();  // Empêcher le comportement par défaut du lien
+            loadPage("clubs");    // Charger le calendrier lorsqu'on clique
+        });
+    }
+    if (newsLink) {
+        newsLink.addEventListener('click', function (e) {
+            e.preventDefault();  // Empêcher le comportement par défaut du lien
+            loadPage("news");    // Charger le calendrier lorsqu'on clique
         });
     }
 });
