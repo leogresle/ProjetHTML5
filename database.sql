ecc3bc8082db2133368f97cee80a39a478d6a6c0
leogresle <leo.gresle@gmail.com>
2025-04-01 13:05:37 +0200

    modification des modules (voir comment.txt)

--- database.sql
+++ database.sql
@@ -5,3 +5,6 @@ CREATE TABLE users (
     role ENUM('admin', 'club') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (email, password, role) 
+VALUES ('leo.gresle@gmail.com', '$2a$10$EXEMPLEHASH1234567890', 'admin');
